12f1e919d69bdea05a6d449e2bc40f3a292e0e5e
DearlingZou8789 <qianfeng@qianfengdeMac-mini-8.local>
2015-04-25 11:32:43 +0800

    add 二叉排序树的删除操作

--- MinispanTree/MinispanTree/search.c
+++ MinispanTree/MinispanTree/search.c
@@ -99,3 +99,59 @@ Status insertBST(BiTree *T, int key)
     else
         return FALSE;   //树中已有关键字相同的结点，不在插入
 }
+
+/*二叉排序树删除操作
+ * 若二叉排序书T中存在关键字等于key的数据元素是，则删除该数据元素结点。
+ * 并返回TRUE,否则返回FALSE。
+ */
+
+Status DeleteBST(BiTree *T, int key)
+{
+    if (!*T)    //不存在关键字等于key的数据元素
+        return FALSE;
+    else
+    {
+        if (key == (*T)->data) //找到关键字等于key的数据元素
+            return Delete(T);
+        else if (key<(*T)->data)
+            return DeleteBST(&(*T)->lchild, key);
+        else
+            return DeleteBST(&(*T)->rchild, key);
+    }
+}
+
+
+/*从二叉排序树中删除结点p，并重接它的左或右子树*/
+Status Delete(BiTree *p)
+{
+    BiTree q, s;
+    if ( (*p)->rchild == NULL )  //右子树空则只需要重接它的左子树
+    {
+        q = *p;
+        *p = (*p)->lchild;
+        free(q);
+    }
+    else if ( (*p)->lchild == NULL )        //只需重接它的右子树
+    {
+        q = *p;
+        *p = (*p)->rchild;
+        free(q);
+    }
+    else        //左右子树均不为空
+    {
+        q = *p;
+        s = (*p)->lchild;
+        while (s->rchild)   //转左，然后向右到尽头（找待删结点的前驱）
+        {
+            q = s;
+            s = s->rchild;
+        }
+        (*p)->data = s->data;       //s指向被删结点的直接前驱
+        if (q!=*p)
+            q->rchild = s->lchild;  //重接q的右子树
+        else
+            q->lchild = s->lchild;  //重接q的左子树
+        free(s);
+    }
+    return TRUE;
+ }
